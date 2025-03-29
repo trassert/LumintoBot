d732a4c5969403b02182a4a50627e675d7d1b476
trassert <s3pple@yandex.ru>
2025-03-29 22:00:08 +0300

    Правка ошибки

--- modules/config.py
+++ modules/config.py
@@ -4,4 +4,4 @@ from os import path
 
 tokens = Config(path.join('configs', 'tokens.yml'))
 coofs = Config(path.join('configs', 'coofs.yml'))
-chats = Config(path.join('configs', 'tokens.yml'))
+chats = Config(path.join('configs', 'chats.yml'))
