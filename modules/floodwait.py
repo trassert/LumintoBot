845e49c37643c29c7b2b52a56b911c643c35ef91
trassert <s3pple@yandex.ru>
2025-06-30 23:09:30 +0300

    Убрана заглушка FloodWait

--- modules/floodwait.py
+++ modules/floodwait.py
@@ -7,7 +7,7 @@ class FloodWaitBase:
     def __init__(self, name="FloodWaitSys", timer=5, lasttime=time()):
         logger.info(f"ФлудВайт: {name} инициализирован")
         self.time = lasttime
-        self.timer = 5
+        self.timer = timer
     def request(self):
         now = time()
         wait = round(now-self.time)
