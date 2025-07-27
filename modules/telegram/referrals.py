7d57021233dfc3bcaa35d22837a06d5137a4d049
trassert <s3pple@yandex.ru>
2025-07-27 20:20:23 +0300

    Новые команды + и - реф (пока ничего не делают)

--- modules/telegram/referrals.py
+++ modules/telegram/referrals.py
@@ -12,4 +12,45 @@ from telethon import errors as TGErrors
 from telethon import events
 from telethon.tl.custom import Message
 
+from .client import client
+from .global_checks import *
+from .func import get_name
 
+
+@client.on(events.NewMessage(pattern=r"(?i)^/addrefcode (.+)", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/новыйреф (.+)", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/новаярефка (.+)", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/новый реф (.+)", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^\+реф (.+)", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^\+рефка (.+)", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^\+рефкод (.+)", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/добавить рефку (.+)", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^добавить рефку (.+)", func=checks))
+async def add_refcode(event: Message):
+    pass
+
+
+@client.on(events.NewMessage(pattern=r"(?i)^/addrefcode$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/новыйреф$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/новаярефка$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/новый реф$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^\+реф$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^\+рефка$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^\+рефкод$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/добавить рефку$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^добавить рефку$", func=checks))
+async def add_refcode_empty(event: Message):
+    pass
+
+
+@client.on(events.NewMessage(pattern=r"(?i)^/delrefcode$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/удалитьреф$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/удалитьрефку$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/удалить реф$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^\-реф$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^\-рефка$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^\-рефкод$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^/удалить рефку$", func=checks))
+@client.on(events.NewMessage(pattern=r"(?i)^удалить рефку$", func=checks))
+async def del_refcode(event: Message):
+    pass
