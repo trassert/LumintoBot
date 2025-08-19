fb826a225aeadc76bfaa35201d528869d50a9c78
trassert <s3pple@yandex.ru>
2025-08-19 17:31:00 +0300

    Асинхронное получение сети в /серв, Часть 1

--- modules/system_info.py
+++ modules/system_info.py
@@ -2,9 +2,22 @@ import psutil
 import platform
 import WinTmp
 from time import time
+from asyncio import sleep
 
 
-def get_system_info():
+
+async def get_current_speed():
+    start_counters = psutil.net_io_counters()
+    await sleep(0.1)
+    end_counters = psutil.net_io_counters()
+    delta_bytes_sent = end_counters.bytes_sent - start_counters.bytes_sent
+    delta_bytes_recv = end_counters.bytes_recv - start_counters.bytes_recv
+    upload_speed_mbps = (delta_bytes_sent / 0.1 * 8) / (1000 * 1000)
+    download_speed_mbps = (delta_bytes_recv / 0.1 * 8) / (1000 * 1000)
+    return [download_speed_mbps, upload_speed_mbps]
+
+
+async def get_system_info():
     boot_time = psutil.boot_time()
     current_time = time()
     uptime_seconds = current_time - boot_time
@@ -21,11 +34,8 @@ def get_system_info():
     mem_total = mem.total / (1024 * 1024 * 1024)
     mem_avail = mem.available / (1024 * 1024 * 1024)
     mem_used = mem.used / (1024 * 1024 * 1024)
-    disk_usage = psutil.disk_usage("/")
-    disk_total = disk_usage.total / (1024 * 1024 * 1024)
-    disk_used = disk_usage.used / (1024 * 1024 * 1024)
-    disk_free = disk_usage.free / (1024 * 1024 * 1024)
     temp = WinTmp.CPU_Temps()
+    network = await get_current_speed()
     return f"""⚙️ : Информация о хостинге:
     Время работы: {result}
     ОС: {platform.system()} {platform.release()}
@@ -35,16 +45,16 @@ def get_system_info():
         Загрузка: {psutil.cpu_percent(0.5)} %
         Температура ↑|≈|↓: {round(max(temp))} | {round(sum(temp)/len(temp))} | {round(min(temp))} 
     Память:
-        Общий объем: {mem_total:.1f} ГБ
+        Объём: {mem_total:.1f} ГБ
         Доступно: {mem_avail:.1f} ГБ
         Используется: {mem_used:.1f} ГБ
         Загрузка: {mem.percent} %
-    Диск:
-        Всего: {disk_total:.1f} ГБ
-        Используется: {disk_used:.1f} ГБ
-        Свободно: {disk_free:.1f} ГБ
-        Загрузка: {disk_usage.percent} %
+    Сеть:
+        Загрузка: {network[0]} Мбит/с
+        Выгрузка: {network[1]} Мбит/с
     """
 
 if __name__ == "__main__":
-    print(get_system_info())
+    async def main():
+        print(await get_system_info())
+    asyncio.run(main())
