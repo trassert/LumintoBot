b0528641897b1263128ebdfc039e8044ae4fd777
trassert <trassert86@gmail.com>
2025-10-12 14:56:00 +0300

    /профиль теперь показывает место в топе

--- modules/telegram/base.py
+++ modules/telegram/base.py
@@ -120,8 +120,14 @@ async def profile(event: Message):
                         f"papi parse --null %PTM_playtime_{nick}:luminto%"
                     )
                 ).replace("\n", "")
+                rank = (
+                    await rcon.send(
+                        f"papi parse --null %PTM_rank_{nick}%"
+                    )
+                ).replace("\n", "")
         except Exception:
             time = "Неизвестно"
+            rank = "Неизвестно"
     else:
         m_day = 0
         m_week = 0
@@ -129,6 +135,7 @@ async def profile(event: Message):
         m_all = 0
         nick = "Не привязан"
         time = "-"
+        rank = "-"
     return await event.reply(
         phrase.profile.full.format(
             name=await get_name(event.sender_id, push=False),
@@ -144,6 +151,7 @@ async def profile(event: Message):
                 await db.get_money(event.sender_id), "изумруд"
             ),
             time=time,
+            rank=rank
         )
     )
 
