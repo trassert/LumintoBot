7013cdd052c59cf30eddaa05f393f065b6db4560
trassert <s3pple@yandex.ru>
2026-02-16 20:49:31 +0300

    Переделка конфига.

--- modules/config.py
+++ modules/config.py
@@ -1,10 +1,42 @@
-from bestconfig import Config
+from pathlib import Path
+
+import yaml
 from loguru import logger
 
 from . import pathes
 
 logger.info(f"Загружен модуль {__name__}!")
 
-cfg = Config(pathes.config / "config.yml")
-tokens = Config(pathes.config / "tokens.yml")
-chats = Config(pathes.config / "chats.yml")
+
+class ConfigSection(dict):
+    "Конфиг-секции для менеджера. Dict -> ConfigSection."
+
+    def __init__(self, data):
+        super().__init__(data)
+        for key, value in data.items():
+            if isinstance(value, dict):
+                self[key] = ConfigSection(value)
+            elif isinstance(value, list):
+                self[key] = [
+                    ConfigSection(i) if isinstance(i, dict) else i for i in value
+                ]
+
+    def __getattr__(self, key):
+        return self.get(key)
+
+
+class ConfigManager:
+    "Root. Вызывается."
+
+    def __init__(self, path: Path):
+        logger.info(f"Зарегестрирован конфиг {path}")
+        raw_data = yaml.safe_load(path.read_text(encoding="utf-8"))
+        self._data = ConfigSection(raw_data)
+
+    def __getattr__(self, key):
+        return getattr(self._data, key)
+
+
+cfg = ConfigManager(pathes.config / "config.yml")
+tokens = ConfigManager(pathes.config / "tokens.yml")
+chats = ConfigManager(pathes.config / "chats.yml")
